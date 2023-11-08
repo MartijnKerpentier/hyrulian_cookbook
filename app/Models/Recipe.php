f0b7a475287fe288f0e9080132a9411bccecf51e
MartijnKerpentier <martijnkerpentier162339@horizoncollege.nl>
2023-11-08 15:34:29 +0100

    upload models

---
+++ app/Models/Recipe.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'ingredients',
+        'image_url',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
